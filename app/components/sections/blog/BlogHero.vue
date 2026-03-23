2eabc9f0dbee8e4db7b6cd8c7c2956a00a34143e
Gantsev KS <gantsev_ks@octonica.ru>
2026-03-23 14:26:25 +0500

    feat(blog): add hero section

---
+++ app/components/sections/blog/BlogHero.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UPageHero>
+    <template #title>
+      <span class="text-white">
+        <span class="text-primary"> Полезные статьи - </span>
+        просто о сложном
+      </span>
+    </template>
+
+    <template #description>
+      Простым языком о настройке Windows и Linux, лечении вирусов, автоматизации
+      задач и моём опыте. Никакой воды — только то, что работает.
+    </template>
+  </UPageHero>
+</template>
+
+<style lang="css" scoped></style>
--- app/pages/blog.vue
+++ app/pages/blog.vue
@@ -12,7 +12,7 @@ useSchemaOrg([
 
 <template>
   <UPage>
-    <h1>Blog page</h1>
+    <BlogHero />
   </UPage>
 </template>
 
